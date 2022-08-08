09a61ea4bd1295f8de97f4e9cc97a59e558412bb
Juan Ruben Nolasco Silva <jrnolasco@gmail.com>
2022-08-08 06:48:59 -0500

    Avance de metodo para consultar precio historico

--- prices/src/main/java/systems/nolasco/broker/coins/control/CoingeckoCoinsRestClient.java
+++ prices/src/main/java/systems/nolasco/broker/coins/control/CoingeckoCoinsRestClient.java
@@ -1,5 +1,7 @@
 package systems.nolasco.broker.coins.control;
 
+import java.text.MessageFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,15 +11,19 @@ import systems.nolasco.broker.coins.entity.Coin;
 
 @Component
 public class CoingeckoCoinsRestClient {
-    @Value("${app.coingecko.base.url}")
-    private String url;
+	@Value("${app.coingecko.base.url}")
+	private String url;
 
-    @Autowired
-    private RestTemplate restTemplate;
-    
-    public Coin[] getAllCoins()
-    {
-	return restTemplate.getForEntity(url + "/coins/list", Coin[].class).getBody();
-    }
-    
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public Coin[] getAllCoins() {
+		return restTemplate.getForEntity(url + "/coins/list", Coin[].class).getBody();
+	}
+	
+//	public Coin[] getMarketChartRange(String id, String vsCurrency, String from, String to) {
+//		final String request = MessageFormat.format(url + "{0}"|, getAllCoins())
+//		return restTemplate.getForEntity(url + "/coins/list", Coin[].class).getBody();
+//	}
+
 }
